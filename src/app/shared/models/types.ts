566bb509fdd0fc7c3c5caadbfca181b3a9a246bc
Fortune Maseko <fortune@fortune-maseko.co.za>
2019-10-15 09:59:18 +0200

    Remove login error custom type

--- src/app/shared/models/types.ts
+++ src/app/shared/models/types.ts
@@ -1,4 +1,3 @@
 export type TransactionType = 'withdraw' | 'deposit';
-export type LoginError = 'EMAIL_NOT_FOUND' | 'INVALID_PASSWORD';
 export type AccountActionType = 'manage' | 'add';
 export type OverdraftLimitUpdate = 'increase-overdraft' | 'decrease-overdraft';
